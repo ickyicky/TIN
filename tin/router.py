36de391197dfcab0e9bdaca901d3e010d82975ab
doman <domanski@basement.pl>
2020-06-03 21:28:04 +0200

    directory name cannot contain dot character

--- tin/router.py
+++ tin/router.py
@@ -8,9 +8,9 @@ router.register(("POST", r"/file/(?P<file_path>[\/A-Za-z0-9\.\_]+)", FilePost))
 router.register(("GET", r"/file/(?P<file_path>[\/A-Za-z0-9\.\_]+)", FileGet))
 router.register(("DELETE", r"/file/(?P<file_path>[\/A-Za-z0-9\.\_]+)", FileDelete))
 
-router.register(("POST", r"/dir/(?P<dir_path>[\/A-Za-z0-9\.\_]+)", MakeDir))
-router.register(("GET", r"/dir/(?P<dir_path>[\/A-Za-z0-9\.\_]+)", ListDir))
-router.register(("DELETE", r"/dir/(?P<dir_path>[\/A-Za-z0-9\.\_]+)", DeleteDir))
+router.register(("POST", r"/dir/(?P<dir_path>[\/A-Za-z0-9\_]+)", MakeDir))
+router.register(("GET", r"/dir/(?P<dir_path>[\/A-Za-z0-9\_]+)", ListDir))
+router.register(("DELETE", r"/dir/(?P<dir_path>[\/A-Za-z0-9\_]+)", DeleteDir))
 router.register(("GET", r"/dir", ListDir))
 
 router.register(("POST", r"/authorize", authorize))
